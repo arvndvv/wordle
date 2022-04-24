f139058841d8491581b1be9b2ef90df9ebeb417a
arvndvv <arvndvv@gmail.com>
2022-04-24 17:19:08 +0530

    with keyboard

--- src/components/handleWords/handleWords.component.jsx
+++ src/components/handleWords/handleWords.component.jsx
@@ -1,5 +1,6 @@
-import React,{useEffect,useState,useRef} from 'react';
+import React,{useEffect,useState} from 'react';
 import './handleWords.styles.css';
+import clearImg from '../../assets/img/backspace.svg';
 const HandleWords = ()=>{
     const alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     const randomWordApi = 'https://random-word-api.herokuapp.com/all';
@@ -8,7 +9,11 @@ const HandleWords = ()=>{
     const [currentIndex, setCurrentIndex] = useState(0);
     const [wordle,setWordle] = useState('');
     const [toaster,setToaster] = useState('');
-    const inputRef = useRef();
+    const [keyBoard,setKeyBoard] = useState([
+        [{value:'q',class:'inactive'},{value:'w',class:'inactive'},{value:'e',class:'inactive'},{value:'r',class:'inactive'},{value:'t',class:'inactive'},{value:'y',class:'inactive'},{value:'u',class:'inactive'},{value:'i',class:'inactive'},{value:'o',class:'inactive'},{value:'p',class:'inactive'}],
+        [{value:'a',class:'inactive'},{value:'s',class:'inactive'},{value:'d',class:'inactive'},{value:'f',class:'inactive'},{value:'g',class:'inactive'},{value:'h',class:'inactive'},{value:'j',class:'inactive'},{value:'k',class:'inactive'},{value:'l',class:'inactive'}],
+        [{value:'enter',class:'inactive'},{value:'z',class:'inactive'},{value:'x',class:'inactive'},{value:'c',class:'inactive'},{value:'v',class:'inactive'},{value:'b',class:'inactive'},{value:'n',class:'inactive'},{value:'m',class:'inactive'},{value:'backspace',class:'inactive'}]
+    ]);
     const [inputMatrix,setinputMatrix]=useState([
         [{letter:'',class:'inActive'},{letter:'',class:'inActive'},{letter:'',class:'inActive'},{letter:'',class:'inActive'},{letter:'',class:'inActive'}],
         [{letter:'',class:'inActive'},{letter:'',class:'inActive'},{letter:'',class:'inActive'},{letter:'',class:'inActive'},{letter:'',class:'inActive'}],
@@ -52,90 +57,128 @@ const HandleWords = ()=>{
             setWordle(word)
         })()
     }, []);
-    useEffect(() => {
-        const handleKeyDown = async(e)=>{
-            if(!wordle.length || currentIndex === 6){
-                return;
+    const handleKeyDown = async(e)=>{
+        if(!wordle.length || currentIndex === 6){
+            return;
+        }
+        const keyPressed = e.key.toLowerCase();
+        if(!alphabets.includes(keyPressed)){
+            if(keyPressed==='backspace' && currentInput.length){
+                setCurrentInput(currentInput.slice(0,-1))
             }
-            const keyPressed = e.key.toLowerCase();
-            if(!alphabets.includes(keyPressed)){
-                if(keyPressed==='backspace' && currentInput.length){
-                    setCurrentInput(currentInput.slice(0,-1))
+            if(keyPressed==='enter' && currentInput.length === 5){
+                const wordExist = await checkIfWordExist(currentInput);
+                if(!wordExist){
+                    showToaster('Not in wordlist')
+                    return;
                 }
-                if(keyPressed==='enter' && currentInput.length === 5){
-                    const wordExist = await checkIfWordExist(currentInput);
-                    if(!wordExist){
-                        showToaster('Not in wordlist')
-                        return;
-                    }
-                    const lowerCaseInput = currentInput.toLowerCase();
-                    const currentInputArray = lowerCaseInput.split('');
-                    let wordsHistory = inputMatrix;
-                    let inputMatrixCurrentRow = wordsHistory[currentIndex];
-                    currentInputArray.forEach((letter,index)=>{
-                        let className = 'not-match';
-                        if(wordle[index] === letter){
-                            className = 'exact-match'
-                        }
-                        else if(wordle.includes(letter)){
-                            const countOfLetterInWordle = (wordle.match(new RegExp(letter, "g")) || []).length;
-                            const countOfLetterInInput = (lowerCaseInput.match(new RegExp(letter, "g")) || []).length;
-                            if(countOfLetterInWordle >= countOfLetterInInput){
-                                className = 'scrambled'
-                            }
-                            else{
-                                const InputHasLettersInExactPlaces = wordle.split('').every((wordleLetter,index)=>{
-                                    if(wordleLetter===letter){
-                                       return currentInputArray[index]===letter;
-                                    }
-                                    else{
-                                        return true;
-                                    }
-                                })
-                                if(!InputHasLettersInExactPlaces){
-                                    const AlreadyMatched = inputMatrixCurrentRow.filter(col=>col.letter===letter);
-                                    if(AlreadyMatched.length < countOfLetterInWordle){
-                                    className = 'scrambled'
-                                    }
+                const lowerCaseInput = currentInput.toLowerCase();
+                const currentInputArray = lowerCaseInput.split('');
+                let wordsHistory = inputMatrix;
+                let inputMatrixCurrentRow = wordsHistory[currentIndex];
+                let keyBoardDummy = keyBoard;
+                currentInputArray.forEach((letter,index)=>{
+                    let className = 'not-match';
+                    console.log(letter);
+                    if(wordle[index] === letter){
+                        className = 'exact-match';
+                        keyBoardDummy = keyBoardDummy.map(row =>(
+                            row.map(col=>{
+                                if(col.value===letter && col.class!=='match'){
+                                    return {...col,class:'exact-match'}
                                 }
+                                return col
+                            })
+                        ));
+                    }
+                    else if(wordle.includes(letter)){
+                        const countOfLetterInWordle = (wordle.match(new RegExp(letter, "g")) || []).length;
+                        const countOfLetterInInput = (lowerCaseInput.match(new RegExp(letter, "g")) || []).length;
+                        if(countOfLetterInWordle >= countOfLetterInInput){
+                            className = 'scrambled';
+                            keyBoardDummy = keyBoardDummy.map(row =>(
+                                row.map(col=>{
+                                    if(col.value===letter && col.class!=='match'){
+                                        return {...col,class:'scrambled'}
+                                    }
+                                    return col
+                                })
+                            ));
+
+                        }
+                        else{
+                            const InputHasLettersInExactPlaces = wordle.split('').every((wordleLetter,index)=>{
+                                if(wordleLetter===letter){
+                                   return currentInputArray[index]===letter;
+                                }
+                                else{
+                                    return true;
+                                }
+                            })
+                            if(!InputHasLettersInExactPlaces){
+                                const AlreadyMatched = inputMatrixCurrentRow.filter(col=>col.letter===letter);
+                                if(AlreadyMatched.length < countOfLetterInWordle){
+                                className = 'scrambled';
+                                keyBoardDummy = keyBoardDummy.map(row =>(
+                                    row.map(col=>{
+                                        if(col.value===letter && col.class!=='match'){
+                                            return {...col,class:'scrambled'}
+                                        }
+                                        return col
+                                    })
+                                ));
+                                }
+
                             }
                         }
-                        inputMatrixCurrentRow[index].letter=letter;
-                        inputMatrixCurrentRow[index].class = className;
-                    })
-                    setinputMatrix(wordsHistory);
-                    if(currentInput===wordle){
-                        setCurrentIndex(6);
-                        setCurrentInput('');
-                        //success
-                        showToaster('Genius! You guess it right!',true);
-                        return;
                     }
-                    setCurrentIndex(currentIndex+1);
+                    if(className==='not-match'){
+                        keyBoardDummy = keyBoardDummy.map(row =>(
+                            row.map(col=>{
+                                if(col.value===letter && col.class!=='match'){
+                                    return {...col,class:'not-match'}
+                                }
+                                return col
+                            })
+                        ));
+                        
+                    }
+                    inputMatrixCurrentRow[index].letter=letter;
+                    inputMatrixCurrentRow[index].class = className;
+                })
+                setKeyBoard(keyBoardDummy);
+                setinputMatrix(wordsHistory);
+                if(currentInput===wordle){
+                    setCurrentIndex(6);
                     setCurrentInput('');
-                    if(currentIndex===5){
-                        showToaster(wordle.toUpperCase(),true)
-                    }
+                    //success
+                    showToaster('Genius! You guess it right!',true);
+                    return;
                 }
-                return;
+                setCurrentIndex(currentIndex+1);
+                setCurrentInput('');
+                if(currentIndex===5){
+                    showToaster(wordle.toUpperCase(),true)
+                }
             }
-            if(currentInput.length===5){
-                return;
-            }
-            setCurrentInput(currentInput+keyPressed)
-          }
+            return;
+        }
+        if(currentInput.length===5){
+            return;
+        }
+        setCurrentInput(currentInput+keyPressed)
+      }
+    useEffect(() => {
       
-          inputRef.current.addEventListener('keydown', handleKeyDown);
-      
+          document.addEventListener('keydown', handleKeyDown);
           return function cleanup() {
-            inputRef.current.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keydown', handleKeyDown);
           }
     }, [currentInput,wordle]);
 return(
     <div className='main_wrapper'>
-        <input type="text" ref={inputRef}/>
         <div className='wrapper'>
-        <h1>WORDLEf</h1>
+        <h1>WORDLE</h1>
         <p>{wordle?'Type your Guess':'Getting word for you...'}</p>
 
         <div className={"wordle " + (!wordle?'isloading':'')}>
@@ -159,6 +202,21 @@ return(
             }
         </div>
         </div>
+        <div className="keyboard">
+            {
+                keyBoard.map((row,index)=>(
+                    <div className="row" key={index}>
+                        {
+                            row.map((col,colIndex)=>(
+                                <div className={'col ' + col.class} key={colIndex} onClick={()=>{handleKeyDown({key:col.value})}}>
+                                    {col.value==='backspace'?(<img src={clearImg} alt="clear"/>): col.value}
+                                </div>
+                            ))
+                        }
+                        </div>
+                ))
+            }
+        </div>
         <div className="credits">
             <strong>APIs Used:</strong>
 
@@ -167,7 +225,6 @@ return(
 
 
         </div>
-        {/* ☕  */}
    <div className="footer">
        <div>
            Drink ☕ & code JS 
@@ -175,6 +232,7 @@ return(
        <div>
        Coded with <span className='heart'>♥</span> <a href='https://arvndvv.github.io'>Aravind</a></div>
        </div>
+       <a target="_blank" href="https://icons8.com/icon/UhociNVMiEEF/clear-symbol">Clear Symbol</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
     </div>
 )
 }
